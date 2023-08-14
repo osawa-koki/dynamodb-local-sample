d95196fc1e2ff3d10b3113314fb66f1374cbbd46
osawa-koki <koki@osawa.dev>
2023-08-14 22:41:01 +0900

    DynamoDBへの接続。

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+import AWS from 'aws-sdk'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+const AWS_DYNAMODB_ENDPOINT = process.env.AWS_DYNAMODB_ENDPOINT!
+const AWS_REGION = process.env.AWS_REGION!
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!
+/* eslint-enable @typescript-eslint/no-non-null-assertion */
+
+const config = {
+  endpoint: AWS_DYNAMODB_ENDPOINT,
+  region: AWS_REGION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY
+}
+AWS.config.update(config)
+const dynamodb = new AWS.DynamoDB()
